f286c9b250339535f5fc44e1639de8d1e48870ac
oz <147579777+thumb26@users.noreply.github.com>
2023-10-16 13:07:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+@app.get("/Welcome")
+def read_root():
+    return {"HEllo": "Welcome"}
+
+
 app.mount("/", StaticFiles(directory="public", html = True), name="static")
